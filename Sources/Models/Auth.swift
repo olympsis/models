0c258f3ef9761eea3f3990da9cfc1f716ac123c8
Joel Joseph <joeljordanjoseph@gmail.com>
2023-05-11 08:43:53 -0600

    published packages. now i am pulling in my personal packages externally.

---
+++ Sources/Models/Auth.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joel Joseph on 5/10/23.
+//
+
+import SwiftBSON
+import Foundation
+
+//public struct
+
+public struct SignInRequest: Codable {
+    
+}
+
+public struct LoginRequest: Codable {
+    
+}
