3511f90d0ab4c2efab0328d71a271e5d75d23855
josephjoeljo <joeljordanjoseph@gmail.com>
2024-05-11 21:49:57 -0600

    tweak

--- auth.go
+++ auth.go
@@ -6,7 +6,6 @@ type AuthUser struct {
 	FirstName *string `json:"first_name,omitempty" bson:"first_name,omitempty"`
 	LastName  *string `json:"last_name,omitempty" bson:"last_name,omitempty"`
 	Email     *string `json:"email,omitempty" bson:"email,omitempty"`
-	Provider  *string `json:"provider,omitempty" bson:"provider,omitempty"`
 	Token     *string `json:"token,omitempty" bson:"token,omitempty"`
 	CreatedAt *int64  `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
