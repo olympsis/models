93355bbc4508c40a1acd33249fd9bf4b5867e406
josephjoeljo <joeljordanjoseph@gmail.com>
2024-05-11 23:49:54 -0600

    added token back so i can verify user token

--- auth.go
+++ auth.go
@@ -14,6 +14,7 @@ type AuthRequest struct {
 	FirstName *string `json:"first_name,omitempty"`
 	LastName  *string `json:"last_name,omitempty"`
 	Email     *string `json:"email,omitempty"`
+	Token     *string `json:"token,omitempty"`
 	UUID      *string `json:"uuid,omitempty"`
 }
 
