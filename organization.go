bda5b782160f781b189b684cf9e4f5c9402d3efa
josephjoeljo <joeljordanjoseph@gmail.com>
2024-05-28 15:53:11 -0600

    added bson

--- organization.go
+++ organization.go
@@ -4,21 +4,21 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Object represents groups that clubs can organize under for a hierarchy
 type Organization struct {
-	ID          primitive.ObjectID   `json:"id,omitempty"`
-	Name        string               `json:"name,omitempty"`
-	Description string               `json:"description,omitempty"`
-	Sports      []string             `json:"sports,omitempty"`
-	City        string               `json:"city,omitempty"`
-	State       string               `json:"state,omitempty"`
-	Country     string               `json:"country,omitempty"`
-	Logo        string               `json:"logo,omitempty"`
-	Banner      string               `json:"banner,omitempty"`
-	Members     []Member             `json:"members,omitempty"`
-	Children    []ClubDao            `json:"children,omitempty"`
-	BlackList   []string             `json:"black_list,omitempty"`
-	PinnedPosts []primitive.ObjectID `json:"pinned_posts,omitempty"`
-	IsVerified  bool                 `json:"is_verified,omitempty"`
-	CreatedAt   int64                `json:"created_at,omitempty"`
+	ID          primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+	Name        string               `json:"name,omitempty" bson:"name,omitempty"`
+	Description string               `json:"description,omitempty" bson:"description,omitempty"`
+	Sports      []string             `json:"sports,omitempty" bson:"sports,omitempty"`
+	City        string               `json:"city,omitempty" bson:"city,omitempty"`
+	State       string               `json:"state,omitempty" bson:"state,omitempty"`
+	Country     string               `json:"country,omitempty" bson:"country,omitempty"`
+	Logo        string               `json:"logo,omitempty" bson:"logo,omitempty"`
+	Banner      string               `json:"banner,omitempty" bson:"banner,omitempty"`
+	Members     []Member             `json:"members,omitempty" bson:"members,omitempty"`
+	Children    []ClubDao            `json:"children,omitempty" bson:"children,omitempty"`
+	BlackList   []string             `json:"black_list,omitempty" bson:"black_list,omitempty"`
+	PinnedPosts []primitive.ObjectID `json:"pinned_posts,omitempty" bson:"pinned_posts,omitempty"`
+	IsVerified  bool                 `json:"is_verified,omitempty" bson:"is_verified,omitempty"`
+	CreatedAt   int64                `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
 
 // Data access object for the organizations
