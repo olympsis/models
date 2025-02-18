094241c09519a0f51cc957c1a51afc8a76e95e8f
josephjoeljo <joeljordanjoseph@gmail.com>
2025-02-18 14:10:23 -0500

    handle updates for notif topics

--- notif.go
+++ notif.go
@@ -134,8 +134,9 @@ type NotificationItem struct {
 NotificationUpdateRequest for marking notifications as read/archived
 */
 type NotificationUpdateRequest struct {
-	NotificationIDs []primitive.ObjectID `json:"notification_ids"`
-	Action          string               `json:"action"` // read, unread, archive
+	NotificationIDs []primitive.ObjectID  `json:"notification_ids"`
+	Action          string                `json:"action"` // read, unread, archive
+	Updates         *NotificationTopicDao `json:"updates,omitempty"`
 }
 
 /*
