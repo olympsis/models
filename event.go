409a78fa828e2f1eafcb5468a32fd6a657c42357
josephjoeljo <joeljordanjoseph@gmail.com>
2025-03-18 15:45:50 -0400

    event models overhaul

--- event.go
+++ event.go
@@ -3,54 +3,102 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Event struct {
-	ID              primitive.ObjectID `json:"id" bson:"_id"`
-	Type            int8               `json:"type" bson:"type"`
-	Poster          UserSnippet        `json:"poster" bson:"poster"`
-	Organizers      []Organizer        `json:"organizers" bson:"organizers"`
-	Venues          []VenueDescriptor  `json:"venues" bson:"venues"`
-	ImageURL        string             `json:"image_url" bson:"image_url"`
-	Title           string             `json:"title" bson:"title"`
-	Body            string             `json:"body" bson:"body"`
-	Sports          []string           `json:"sports" bson:"sports"`
-	Level           int8               `json:"level" bson:"level"`
-	StartTime       int64              `json:"start_time" bson:"start_time"`
-	StopTime        int64              `json:"stop_time" bson:"stop_time"`
-	MinParticipants *int32             `json:"min_participants,omitempty" bson:"min_participants,omitempty"`
-	MaxParticipants *int32             `json:"max_participants,omitempty" bson:"max_participants,omitempty"`
-	Participants    *[]Participant     `json:"participants,omitempty" bson:"participants,omitempty"`
-	WaitList        *[]Participant     `json:"wait_list,omitempty" bson:"wait_list,omitempty"`
-	Visibility      int8               `json:"visibility" bson:"visibility"`
-	ExternalLink    *string            `json:"external_link,omitempty" bson:"external_link,omitempty"`
-	IsSensitive     bool               `json:"is_sensitive" bson:"is_sensitive"`
-	CreatedAt       int64              `json:"created_at" bson:"created_at"`
+	ID         primitive.ObjectID `json:"id" bson:"_id"`
+	Poster     UserSnippet        `json:"poster" bson:"poster"`
+	Organizers []Organizer        `json:"organizers" bson:"organizers"`
+	Venues     []VenueDescriptor  `json:"venues" bson:"venues"`
 
-	IsRecurring *bool `json:"is_recurring,omitempty" bson:"is_recurring,omitempty"`
+	MediaURL  string    `json:"media_url" bson:"media_url"`
+	MediaType MediaType `json:"media_type" bson:"media_type"`
+
+	Title  string   `json:"title" bson:"title"`
+	Body   string   `json:"body" bson:"body"`
+	Sports []string `json:"sports" bson:"sports"`
+
+	FormatConfig *EventFormatConfig `json:"format_config,omitempty" bson:"format_config,omitempty"`
+
+	StartTime primitive.DateTime   `json:"start_time" bson:"start_time"`
+	StopTime  primitive.DateTime   `json:"stop_time" bson:"stop_time"`
+	Comments  []primitive.ObjectID `json:"comments" bson:"comments"`
+
+	ParticipantsConfig *ParticipantsConfig   `json:"participants_config" bson:"participants_config"`
+	ParticipantIDs     *[]primitive.ObjectID `json:"participant_ids,omitempty" bson:"participant_ids,omitempty"`
+	WaitlistIDs        *[]primitive.ObjectID `json:"waitlist_ids,omitempty" bson:"waitlist_ids,omitempty "`
+
+	TeamsConfig     *TeamsConfig          `json:"teams_config,omitempty" bson:"teams_config,omitempty"`
+	TeamIDs         *[]primitive.ObjectID `json:"team_ids,omitempty" bson:"team_ids,omitempty"`
+	TeamWaitlistIDs *[]primitive.ObjectID `json:"team_waitlist_ids,omitempty" bson:"team_waitlist_ids,omitempty"`
+
+	Visibility   VisibilityScope `json:"visibility" bson:"visibility"`
+	ExternalLink *string         `json:"external_link,omitempty" bson:"external_link,omitempty"`
+	IsSensitive  bool            `json:"is_sensitive" bson:"is_sensitive"`
+
+	CreatedAt   primitive.DateTime  `json:"created_at" bson:"created_at"`
+	UpdatedAt   primitive.DateTime  `json:"updated_at" bson:"updated_at"`
+	CancelledAt *primitive.DateTime `json:"cancelled_at,omitempty" bson:"cancelled_at,omitempty"`
+
+	RecurrenceConfig *EventRecurrenceConfig `json:"recurrence_config,omitempty" bson:"recurrence_config,omitempty"`
 }
 
 type EventDao struct {
-	Type            *int8              `json:"type,omitempty" bson:"type,omitempty"`
-	Poster          *string            `json:"poster,omitempty" bson:"poster,omitempty"`
-	Organizers      *[]Organizer       `json:"organizers,omitempty" bson:"organizers,omitempty"`
-	Venues          *[]VenueDescriptor `json:"venues,omitempty" bson:"venues,omitempty"`
-	ImageURL        *string            `json:"image_url,omitempty" bson:"image_url,omitempty"`
-	Title           *string            `json:"title,omitempty" bson:"title,omitempty"`
-	Body            *string            `json:"body,omitempty" bson:"body,omitempty"`
-	Sports          *[]string          `json:"sports,omitempty" bson:"sports,omitempty"`
-	Level           *int8              `json:"level,omitempty" bson:"level,omitempty"`
-	StartTime       *int64             `json:"start_time,omitempty" bson:"start_time,omitempty"`
-	StopTime        *int64             `json:"stop_time,omitempty" bson:"stop_time,omitempty"`
-	MinParticipants *int32             `json:"min_participants,omitempty" bson:"min_participants,omitempty"`
-	MaxParticipants *int32             `json:"max_participants,omitempty" bson:"max_participants,omitempty"`
-	Participants    *[]ParticipantDao  `json:"participants,omitempty" bson:"participants,omitempty"`
-	WaitList        *[]ParticipantDao  `json:"wait_list,omitempty" bson:"wait_list,omitempty"`
-	Visibility      *int8              `json:"visibility,omitempty" bson:"visibility,omitempty"`
-	ExternalLink    *string            `json:"external_link,omitempty" bson:"external_link,omitempty"`
-	IsSensitive     *bool              `json:"is_sensitive,omitempty" bson:"is_sensitive,omitempty"`
-	CreatedAt       *int64             `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	Poster     *string            `json:"poster,omitempty" bson:"poster,omitempty"`
+	Organizers *[]Organizer       `json:"organizers,omitempty" bson:"organizers,omitempty"`
+	Venues     *[]VenueDescriptor `json:"venues,omitempty" bson:"venues,omitempty"`
 
-	IsRecurring      *bool                 `json:"is_recurring,omitempty" bson:"is_recurring,omitempty"`
+	MediaURL  string    `json:"media_url,omitempty" bson:"media_url,omitempty"`
+	MediaType MediaType `json:"media_type,omitempty" bson:"media_type,omitempty"`
+
+	Title  *string   `json:"title,omitempty" bson:"title,omitempty"`
+	Body   *string   `json:"body,omitempty" bson:"body,omitempty"`
+	Sports *[]string `json:"sports,omitempty" bson:"sports,omitempty"`
+
+	FormatConfig *EventFormatConfig `json:"format_config,omitempty" bson:"format_config,omitempty"`
+
+	StartTime *primitive.DateTime  `json:"start_time,omitempty" bson:"start_time,omitempty"`
+	StopTime  *primitive.DateTime  `json:"stop_time,omitempty" bson:"stop_time,omitempty"`
+	Comments  []primitive.ObjectID `json:"comments,omitempty" bson:"comments,omitempty"`
+
+	ParticipantsConfig *ParticipantsConfig   `json:"participants_config" bson:"participants_config"`
+	ParticipantIDs     *[]primitive.ObjectID `json:"participant_ids,omitempty" bson:"participant_ids,omitempty"`
+	WaitlistIDs        *[]primitive.ObjectID `json:"waitlist_ids,omitempty" bson:"waitlist_ids,omitempty"`
+
+	TeamsConfig     *TeamsConfig          `json:"teams_config,omitempty" bson:"teams_config,omitempty"`
+	TeamIDs         *[]primitive.ObjectID `json:"team_ids,omitempty" bson:"team_ids,omitempty"`
+	TeamWaitlistIDs *[]primitive.ObjectID `json:"team_waitlist_ids,omitempty" bson:"team_waitlist_ids,omitempty"`
+
+	Visibility   *VisibilityScope `json:"visibility,omitempty" bson:"visibility,omitempty"`
+	ExternalLink *string          `json:"external_link,omitempty" bson:"external_link,omitempty"`
+	IsSensitive  *bool            `json:"is_sensitive,omitempty" bson:"is_sensitive,omitempty"`
+
+	CreatedAt   *primitive.DateTime `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt   *primitive.DateTime `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	CancelledAt *primitive.DateTime `json:"cancelled_at,omitempty" bson:"cancelled_at,omitempty"`
+
+	RecurrenceConfig *EventRecurrenceConfig `json:"recurrence_config,omitempty" bson:"recurrence_config,omitempty"`
+}
+
+type EventFormatConfig struct {
+	// Type identifiers
+	IsCompetition       *bool               `json:"is_competition,omitempty" bson:"is_competition,omitempty"`
+	IsCompetitionGame   *bool               `json:"is_competition_game,omitempty" bson:"is_competition_game,omitempty"`
+	ParentCompetitionID *primitive.ObjectID `json:"parent_competition_id,omitempty" bson:"parent_competition_id,omitempty"`
+	CompetitionState    *string             `json:"competition_state,omitempty" bson:"competition_state,omitempty"` // "not_started", "in_progress", "completed"
+
+	// Format details
+	Format       *CompetitionFormats `json:"format,omitempty" bson:"format,omitempty"`
+	Rounds       *int32              `json:"rounds,omitempty" bson:"rounds,omitempty"`
+	CurrentRound *int32              `json:"current_round,omitempty" bson:"current_round,omitempty"`
+	BracketData  *any                `json:"bracket_data,omitempty" bson:"bracket_data,omitempty"`
+
+	// Registration period (if you decide to include it here)
+	RegistrationStart     *primitive.DateTime `json:"registration_start,omitempty" bson:"registration_start,omitempty"`
+	RegistrationEnd       *primitive.DateTime `json:"registration_end,omitempty" bson:"registration_end,omitempty"`
+	AllowLateRegistration *bool               `json:"allow_late_registration,omitempty" bson:"allow_late_registration,omitempty"`
+}
+
+type EventRecurrenceConfig struct {
 	RecurrenceRule   *string               `json:"recurrence_rule,omitempty" bson:"recurrence_rule,omitempty"`
-	RecurrenceEnd    *int64                `json:"recurrence_end,omitempty" bson:"recurrence_end,omitempty"`
+	RecurrenceEnd    *primitive.DateTime   `json:"recurrence_end,omitempty" bson:"recurrence_end,omitempty"`
 	ParentEventID    *primitive.ObjectID   `json:"parent_event_id,omitempty" bson:"parent_event_id,omitempty"`
 	DeletedInstances *[]primitive.ObjectID `json:"deleted_instances,omitempty" bson:"deleted_instances,omitempty"`
 }
@@ -61,17 +109,47 @@ type EventsResponse struct {
 }
 
 type Participant struct {
-	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id"`
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	User      *UserSnippet       `json:"user,omitempty" bson:"user,omitempty"`
-	Status    int8               `json:"status" bson:"status"`
-	CreatedAt int64              `json:"created_at,omitempty" bson:"created_at"`
+	Status    RSVPStatus         `json:"status" bson:"status"`
+	EventID   primitive.ObjectID `json:"event_id" bson:"event_id"`
+	CreatedAt primitive.DateTime `json:"created_at,omitempty" bson:"created_at"`
 }
 
 type ParticipantDao struct {
 	ID        *primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	UUID      *string             `json:"uuid,omitempty" bson:"uuid,omitempty"`
-	Status    *int8               `json:"status,omitempty" bson:"status,omitempty"`
-	CreatedAt *int64              `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	Status    *RSVPStatus         `json:"status,omitempty" bson:"status,omitempty"`
+	EventID   *primitive.ObjectID `json:"event_id,omitempty" bson:"event_id,omitempty"`
+	CreatedAt *primitive.DateTime `json:"created_at,omitempty" bson:"created_at,omitempty"`
+}
+
+type ParticipantsConfig struct {
+	HasWaitlist     *bool  `json:"has_waitlist,omitempty" bson:"has_waitlist,omitempty"`
+	MinParticipants *int32 `json:"min_participants,omitempty" bson:"min_participants,omitempty"`
+	MaxParticipants *int32 `json:"max_participants,omitempty" bson:"max_participants,omitempty"`
+}
+
+type Team struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	Name      string             `json:"name" bson:"name"`
+	Members   []Participant      `json:"members" bson:"members"`
+	EventID   primitive.ObjectID `json:"event_id" bson:"event_id"`
+	CreatedAt primitive.DateTime `json:"created_at,omitempty" bson:"created_at"`
+}
+
+type TeamDao struct {
+	Name      *string             `json:"name,omitempty" bson:"name,omitempty"`
+	Members   *[]Participant      `json:"members,omitempty" bson:"members,omitempty"`
+	EventID   *primitive.ObjectID `json:"event_id,omitempty" bson:"event_id,omitempty"`
+	CreatedAt *primitive.DateTime `json:"created_at,omitempty" bson:"created_at,omitempty"`
+}
+
+type TeamsConfig struct {
+	HasWaitlist *bool  `json:"has_waitlist,omitempty" bson:"has_waitlist,omitempty"`
+	MinTeams    *int32 `json:"min_teams,omitempty" bson:"min_teams,omitempty"`
+	MaxTeams    *int32 `json:"max_teams,omitempty" bson:"max_teams,omitempty"`
+	MaxTeamSize *int32 `json:"max_team_size,omitempty" bson:"max_team_size,omitempty"`
 }
 
 type FullEventError struct {
@@ -91,7 +169,103 @@ type NewEventDao struct {
 }
 
 type RecurrenceOptions struct {
-	Pattern  string `json:"pattern"`  // "DAILY", "WEEKLY", "MONTHLY"
-	EndTime  int64  `json:"end_time"` // Must be after the event's start_time
-	Interval int    `json:"interval"` // e.g., every 2 weeks
+	Pattern  string             `json:"pattern"`  // "DAILY", "WEEKLY", "MONTHLY"
+	EndTime  primitive.DateTime `json:"end_time"` // Must be after the event's start_time
+	Interval int                `json:"interval"` // e.g., every 2 weeks
+}
+
+type EventComment struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id"`
+	User      *UserSnippet       `json:"user,omitempty" bson:"user,omitempty"`
+	Text      string             `json:"text" bson:"text"`
+	EventID   primitive.ObjectID `json:"event_id" bson:"event_id"`
+	CreatedAt primitive.DateTime `json:"created_at" bson:"created_at"`
+}
+
+type EventCommentDao struct {
+	ID        *primitive.ObjectID `json:"id,omitempty" bson:"_id"`
+	UUID      *string             `json:"uuid,omitempty" bson:"uuid,omitempty"`
+	Text      *string             `json:"text,omitempty" bson:"text,omitempty"`
+	EventID   primitive.ObjectID  `json:"event_id,omitempty" bson:"event_id,omitempty"`
+	CreatedAt *primitive.DateTime `json:"created_at,omitempty" bson:"created_at,omitempty"`
+}
+
+// 						//
+// EVENT LOGGING MODELS //
+//						//
+
+type EventAuditLog struct {
+	ID            primitive.ObjectID `json:"id" bson:"_id"`
+	EventID       primitive.ObjectID `json:"event_id" bson:"event_id"`
+	UserID        string             `json:"user_id" bson:"user_id"`
+	Action        string             `json:"action" bson:"action"` // "create", "update", "cancel", etc.
+	FieldsChanged []string           `json:"fields_changed,omitempty" bson:"fields_changed,omitempty"`
+	OldValues     map[string]any     `json:"old_values,omitempty" bson:"old_values,omitempty"`
+	NewValues     map[string]any     `json:"new_values,omitempty" bson:"new_values,omitempty"`
+	Timestamp     primitive.DateTime `json:"timestamp" bson:"timestamp"`
+}
+
+type EventAnalytics struct {
+	ID      primitive.ObjectID `json:"id" bson:"_id"`
+	EventID primitive.ObjectID `json:"event_id" bson:"event_id"`
+
+	// Overview metrics
+	ViewCount         int32 `json:"view_count" bson:"view_count"`
+	UniqueViewerCount int32 `json:"unique_viewer_count" bson:"unique_viewer_count"`
+
+	// Conversion metrics
+	RSVPCount      int32   `json:"rsvp_count" bson:"rsvp_count"`
+	WaitlistCount  int32   `json:"waitlist_count" bson:"waitlist_count"`
+	ConversionRate float32 `json:"conversion_rate" bson:"conversion_rate"` // ViewToRSVP %
+
+	// Engagement metrics
+	ShareCount   int32 `json:"share_count" bson:"share_count"`
+	SaveCount    int32 `json:"save_count" bson:"save_count"`
+	CommentCount int32 `json:"comment_count" bson:"comment_count"`
+
+	// Time-series data (for charts)
+	DailyMetrics map[string]*DailyEngagement `json:"daily_metrics" bson:"daily_metrics"` // Date string -> metrics
+
+	// Audience demographics (for pie charts)
+	PlatformBreakdown map[string]int32 `json:"platform_breakdown" bson:"platform_breakdown"` // "ios", "android", "web"
+	SourceBreakdown   map[string]int32 `json:"source_breakdown" bson:"source_breakdown"`     // "search", "feed", "direct"
+
+	// For competition events
+	TeamViewMetrics map[string]int32 `json:"team_view_metrics,omitempty" bson:"team_view_metrics,omitempty"` // TeamID -> views
+
+	// Update tracking
+	LastUpdated primitive.DateTime `json:"last_updated" bson:"last_updated"`
+}
+
+type DailyEngagement struct {
+	Views         int32 `json:"views" bson:"views"`
+	UniqueViews   int32 `json:"unique_views" bson:"unique_views"`
+	RSVPs         int32 `json:"rsvps" bson:"rsvps"`
+	Shares        int32 `json:"shares" bson:"shares"`
+	AvgTimeOnPage int32 `json:"avg_time_on_page" bson:"avg_time_on_page"` // seconds
+}
+
+type EventDetailedView struct {
+	ID      primitive.ObjectID `json:"id" bson:"_id"`
+	EventID primitive.ObjectID `json:"event_id" bson:"event_id"`
+
+	// User info (partially anonymized)
+	UserID   *string `json:"user_id,omitempty" bson:"user_id,omitempty"`
+	UserType string  `json:"user_type" bson:"user_type"` // "member", "non-member", "anonymous"
+
+	// Session details
+	ViewTime primitive.DateTime  `json:"view_time" bson:"view_time"`
+	ExitTime *primitive.DateTime `json:"exit_time,omitempty" bson:"exit_time,omitempty"`
+	Duration int32               `json:"duration" bson:"duration"` // Time in seconds
+
+	// View context
+	Source    string `json:"source" bson:"source"`                           // "search", "profile", "feed", "notification"
+	EntryPath string `json:"entry_path" bson:"entry_path"`                   // URL path they came from
+	ExitPath  string `json:"exit_path,omitempty" bson:"exit_path,omitempty"` // Where they went next
+
+	// Technical data
+	DeviceType string `json:"device_type" bson:"device_type"` // "mobile", "tablet", "desktop"
+	Platform   string `json:"platform" bson:"platform"`       // "ios", "android", "web"
+	Country    string `json:"country,omitempty" bson:"country,omitempty"`
+	Region     string `json:"region,omitempty" bson:"region,omitempty"`
 }
---
+++ variables.go
@@ -0,0 +1,81 @@
+package models
+
+type MediaType string
+
+const (
+	ImageMediaType = "image"
+	VideoMediaType = "video"
+)
+
+// Visibility Scope represents the possible visibility states of many models
+type VisibilityScope int
+
+const (
+	PublicVisibilityScope  VisibilityScope = 0
+	PrivateVisibilityScope VisibilityScope = 1
+	GroupVisibilityScope   VisibilityScope = 2
+)
+
+// RSVP status represents the possible status of an rsvp
+type RSVPStatus int
+
+const (
+	Maybe RSVPStatus = 0
+	Yes   RSVPStatus = 1
+)
+
+// Event Competition formats
+type CompetitionFormats string
+
+const (
+	// General Team Sports (Soccer, Basketball, Volleyball, Football, Flag Football, Padel, Pickleball, Badminton, Ping-Pong, Racketball)
+	Bracket           CompetitionFormats = "bracket"            // Knockout-style tournament
+	League            CompetitionFormats = "league"             // Regular season format
+	RoundRobin        CompetitionFormats = "round_robin"        // Each team plays all other teams
+	SingleElimination CompetitionFormats = "single_elimination" // One loss and you're out
+	DoubleElimination CompetitionFormats = "double_elimination" // Two losses before elimination
+	BestOf3           CompetitionFormats = "best_of_3"          // First to win 2 games
+	BestOf5           CompetitionFormats = "best_of_5"          // First to win 3 games
+	WinnerStaysOn     CompetitionFormats = "winner_stays_on"    // Winners keep playing, losers rotate out
+
+	// Team Formats (Soccer, Basketball, Volleyball, Football, Flag Football, Padel, Pickleball, Badminton, Ping-Pong, Racketball)
+	Versus2  CompetitionFormats = "2v2"
+	Versus3  CompetitionFormats = "3v3"
+	Versus5  CompetitionFormats = "5v5"
+	Versus6  CompetitionFormats = "6v6"
+	Versus7  CompetitionFormats = "7v7"
+	Versus8  CompetitionFormats = "8v8"
+	Versus9  CompetitionFormats = "9v9"
+	Versus10 CompetitionFormats = "10v10"
+	Versus11 CompetitionFormats = "11v11"
+
+	// Individual Sports (Running, Cycling)
+	TimeTrial CompetitionFormats = "time_trial" // Athletes race against the clock
+
+	// Running
+	Sprint       CompetitionFormats = "sprint"        // Short-distance race (e.g., 100m, 200m)
+	LongDistance CompetitionFormats = "long_distance" // Longer races (e.g., 5K, 10K, marathon)
+	Relay        CompetitionFormats = "relay"         // Team race with baton passing
+
+	// Cycling
+	RoadRace  CompetitionFormats = "road_race"  // Mass-start long-distance race
+	Criterium CompetitionFormats = "criterium"  // Short circuit, multiple laps
+	StageRace CompetitionFormats = "stage_race" // Multi-day competition (e.g., Tour de France)
+
+	// Golf
+	StrokePlay         CompetitionFormats = "stroke_play"         // Total strokes over the round(s) determine the winner
+	MatchPlay          CompetitionFormats = "match_play"          // Head-to-head format, winning holes instead of strokes
+	Scramble           CompetitionFormats = "scramble"            // Teams play the best shot among their members
+	BestBall           CompetitionFormats = "best_ball"           // Each player plays their ball, best score counts for the team
+	Stableford         CompetitionFormats = "stableford"          // Points awarded based on score per hole
+	SkinsGame          CompetitionFormats = "skins_game"          // Each hole has a prize (skin), won outright by lowest score
+	AlternateShot      CompetitionFormats = "alternate_shot"      // Two-player teams alternate shots on the same ball
+	Shamble            CompetitionFormats = "shamble"             // Similar to scramble but players play from the best tee shot
+	ModifiedStableford CompetitionFormats = "modified_stableford" // Variation of Stableford with adjusted point values
+	Scratch            CompetitionFormats = "scratch"             // No handicaps, raw stroke count matters
+
+	// Climbing
+	Bouldering    CompetitionFormats = "bouldering"     // Short, difficult climbing routes, no ropes
+	LeadClimbing  CompetitionFormats = "lead_climbing"  // Climbing as high as possible on a tall wall
+	SpeedClimbing CompetitionFormats = "speed_climbing" // Race to the top
+)
